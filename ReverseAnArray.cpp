8a9963d8e1c0f9b6d4594107783de5c6f7206856
Suraj Kumar <suraj06022003@gmail.com>
2025-04-27 18:18:03 +0530

    done reversing of array

---
+++ ReverseAnArray.cpp
@@ -0,0 +1,27 @@
+// Reversing the array
+#include<iostream>
+using namespace std;
+
+void reverseArray(int arr[], int size){
+    int left=0, right=size-1;
+    while(left<right){
+        swap(arr[left], arr[right]);
+        left++;
+        right--;
+    }
+    cout<<"\nArray after reversing is: ";
+    for(int i=0; i<size; i++){
+        cout<<arr[i]<<" ";
+    }
+}
+
+int main(){
+    int arr[5]={3,2,5,41,6};
+    int size =5;
+    cout<<"Array before reverse: ";
+    for(int i=0; i<size; i++){
+        cout<<arr[i]<<" ";
+    }
+    reverseArray(arr, size);
+    return 0;
+}
