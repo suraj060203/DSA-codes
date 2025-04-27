2f09cfb06ada2b8af833d11a2312d0545b6b471d
Suraj Kumar <suraj06022003@gmail.com>
2025-04-27 18:44:33 +0530

    some changes

---
+++ Array/ExtremPrintInAnArray.cpp
@@ -0,0 +1,27 @@
+// // Extreme Print in an array.
+#include<iostream>
+using namespace std;
+
+void extremeArray(int arr[], int size){
+    int left=0, right=size-1;
+    while(left<right){
+        swap(arr[left], arr[right]);
+        left++;
+        right--;
+    }
+    cout<<"\nArray after reversing is: ";
+    for(int i=0; i<size; i++){
+        cout<<arr[i]<<" ";
+    }
+}
+
+int main(){
+    int arr[5]={3,2,5,41,6};
+    int size =5;
+    cout<<"Array before reverse: ";
+    for(int i=0; i<size; i++){
+        cout<<arr[i]<<" ";
+    }
+    extremeArray(arr, size); // Calling function
+    return 0;
+}
